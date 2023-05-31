3ba970034c4d7f73ee7df843203accdc1ef16a68
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2023-05-30 21:14:11 -0300

    Update

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
 require("dotenv").config();
 import express, { Request, Response } from "express";
 import morgan from "morgan";
+import cors from "cors";
+import { connectDB, sequelize } from "./db";
+import noteRouter from "./routes";
 
 const app = express();
 
+app.use(express.json({ limit: "10kb" }));
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
 
 app.get("/api/healthchecker", (req: Request, res: Response) => {
   res.status(200).json({
@@ -12,6 +23,8 @@ app.get("/api/healthchecker", (req: Request, res: Response) => {
     message: "Build CRUD API with Node.js and Sequelize",
   });
 });
+
+app.use("/api/notes", noteRouter);
 
 app.all("*", (req: Request, res: Response) => {
   res.status(404).json({
@@ -22,5 +35,10 @@ app.all("*", (req: Request, res: Response) => {
 
 const PORT = 8000;
 app.listen(PORT, async () => {
-  console.log("Server started Successfully");
+  console.log("🚀Server started Successfully");
+  await connectDB();
+  sequelize.sync({ force: false }).then(() => {
+    console.log("✅Synced database successfully...");
+  });
 });
+
